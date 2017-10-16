747b082af1585e109f32110c8f08565945f91f3a
Sadik Messaoudi <sadikano10@gmail.com>
2017-10-16 16:42:01 +0200

    Added Configuration class to read properties file

---
+++ Configuration/src/Configuration.java
@@ -0,0 +1,57 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sadik
+ */
+public class Configuration 
+{
+    private Properties properties = new Properties();
+    public Configuration()
+    {
+        try 
+        {
+            properties.load(new FileInputStream(".."+ File.separator +"Resources"+File.separator+"Configuration.properties"));
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        catch (IOException ex) 
+        {
+            Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public Configuration(String path)
+    {
+        try 
+        {
+            properties.load(new FileInputStream(path));
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        catch (IOException ex) 
+        {
+            Logger.getLogger(Configuration.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public String getPropertie(String propertieName)
+    {
+        return properties.getProperty(propertieName);
+    }
+}
---
+++ Eval3 Server/src/utils/TasksSource.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author Sadik
+ */
+public interface TasksSource 
+{
+    public Runnable getTache() throws InterruptedException;
+    public boolean existTaches();
+    public void recordTache (Runnable r);
+}
